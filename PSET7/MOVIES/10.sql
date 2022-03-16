a5df515555816020a665c1752027d8cf499b1fe0
Aline Castro <92234598+Aline-Castro@users.noreply.github.com>
2022-03-16 13:04:24 -0300

    Create 10.sql

---
+++ PSET7/MOVIES/10.sql
@@ -0,0 +1,9 @@
+-- Em 10.sql, escreva uma consulta SQL para listar os nomes de todas as pessoas que dirigiram um filme que recebeu uma classificação de pelo menos 9,0.
+-- Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada pessoa.
+-- Se uma pessoa dirigiu mais de um filme que recebeu uma classificação de pelo menos 9,0, eles só devem aparecer em seus resultados uma vez.
+
+SELECT DISTINCT name FROM people
+JOIN directors ON people.id = directors.person_id JOIN movies ON directors.movie_id = movies.id JOIN ratings ON movies.id = ratings.movie_id
+WHERE ratings.rating >= "9.0";
+
+
