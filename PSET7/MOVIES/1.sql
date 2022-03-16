7aaf3e5d5b668fc4e2f25874937a01d0b5be6d33
Aline Castro <92234598+Aline-Castro@users.noreply.github.com>
2022-03-16 13:01:06 -0300

    Create 1.sql

---
+++ PSET7/MOVIES/1.sql
@@ -0,0 +1,5 @@
+--escreva uma consulta SQL para listar os títulos de todos os filmes lançados em 2008.
+--Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
+
+
+SELECT title FROM movies Where year = "2008"
