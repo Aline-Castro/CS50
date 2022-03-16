3a3f2813e53648e14eda8124b1d5eca2250a5b34
Aline Castro <92234598+Aline-Castro@users.noreply.github.com>
2022-03-16 13:05:26 -0300

    Create 12.sql

---
+++ PSET7/MOVIES/12.sql
@@ -0,0 +1,14 @@
+-- Em 12.sql, escreva uma consulta SQL para listar os títulos de todos os filmes em que Johnny Depp e Helena Bonham Carter estrelaram juntos.
+-- Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
+-- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Johnny Depp.
+-- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Helena Bonham Carter.
+
+
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+WHERE people.name = "Helena Bonham Carter" 
+INTERSECT
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id
+AND people.name = "Johnny Depp";
+
