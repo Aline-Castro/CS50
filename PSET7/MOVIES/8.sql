e6bce9581791eeac4d172c1bd09c8a6b321f9886
Aline Castro <92234598+Aline-Castro@users.noreply.github.com>
2022-03-16 13:03:40 -0300

    Create 8.sql

---
+++ PSET7/MOVIES/8.sql
@@ -0,0 +1,8 @@
+-- Em 8.sql, escreva uma consulta SQL para listar os nomes de todas as pessoas que estrelaram Toy Story.
+-- Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada pessoa.
+-- Você pode presumir que há apenas um filme no banco de dados com o título Toy Story.
+
+
+
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";
+                        
